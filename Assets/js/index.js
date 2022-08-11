c0a0ce98b826e0e5f32fd8b9d788c5740284f254
Nunder <nathannunderwood@gmail.com>
2022-08-11 16:10:23 -0700

    Kinda figuring it out

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,25 +1,23 @@
-// console.log(api.key);
 
-// console.log(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&&appid=${api.key}`);
-var userSearch = $('#test');
-var submitButton = $('#submit');
 var apiKey = api.key;
-var city = userSearch.val();
-var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var userInput = document.getElementById("test").value;
+var submitButton = $('#submitButton');
 
-fetch(weatherURL)
-.then(function (response) {
-return response.json();
+
+
+submitButton.on('click', function() {
+    var city = document.getElementById("test").value;
+    localStorage.setItem('city', city);
+    console.log(city);
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    console.log(data);
+    })
 })
-.then(function (data) {
-console.log(data);
-});
 
-submitButton.on('submit', displayWeather());
 
-function displayWeather() {
 
-var result = $('#result');
-result.textContent = city;
-localStorage.setItem('city', city);
-};
+    
