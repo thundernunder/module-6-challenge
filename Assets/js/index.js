483643e191317aba7c6c41ebe0688637d9b1abbf
Nunder <nathannunderwood@gmail.com>
2022-08-12 10:25:32 -0700

    Completed pulling values for today forecast

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -6,12 +6,34 @@ console.log(key);
 
 function getWeather() {
   var city = userInput.val();
-  fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=f4ac895478946fb295776c00914a0c9e")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + api.key)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    var location = $('#city-name');
+    location.text(data.city.name);
+    var temp = $('#temperature');
+    temp.text(data.list[0].main.temp);
+    var wind = $('#wind');
+    wind.text(data.list[0].wind.speed);
+    var humidity = $('#humidity');
+    humidity.text(data.list[0].main.humidity);
+    
+  });
 }
 
 // getWeather();
 
 cityButton = $("#submitButton");
 cityButton.on('click', getWeather);
+
+// function getWeather() {
+//   var city = userInput.val();
+//   fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + api.key)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+//   // var temp = $('#city-name');
+//   // temp.textContent = forecast.main.city.name;
+// }
