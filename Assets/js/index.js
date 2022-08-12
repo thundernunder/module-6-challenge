1993ae0f261c41661c983cdb9c648067969f171c
Nunder <nathannunderwood@gmail.com>
2022-08-12 08:55:47 -0700

    Possible success for data pull

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,13 +1,17 @@
-var city;
+var userInput = $("#input-text");
 var key = api.key;
 console.log(key);
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+
+function getWeather() {
+  var city = userInput.val();
+  fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=f4ac895478946fb295776c00914a0c9e")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+}
+
+// getWeather();
+
+cityButton = $("#submitButton");
+cityButton.on('click', getWeather);
