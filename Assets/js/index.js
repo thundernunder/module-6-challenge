d177d5c16a8689b53e690e3279158d022e293db1
Nunder <nathannunderwood@gmail.com>
2022-08-12 12:13:07 -0700

    getting first panel figured out

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -5,6 +5,7 @@ console.log(key);
 
 function getWeather() {
   var city = userInput.val();
+
   fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + api.key)
   .then(function(response) {
     return response.json();
@@ -19,19 +20,23 @@ function getWeather() {
     wind.text(data.list[0].wind.speed);
     var humidity = $('#humidity');
     humidity.text(data.list[0].main.humidity);
-      
-    localStorage.setItem('city', city); 
-
+    
+  localStorage.setItem('city', city); 
   });
+  localStorage.setItem('city', city);
   makeHistoryButton();
 }
 
 function makeHistoryButton() {
-  
-  var rootEl = $('#top-panel');
+  var rootEl = $('#search-buttons');
   var historyButton = $('<button>');
+  historyButton.addClass('button');
   rootEl.append(historyButton);
   historyButton.text(localStorage.getItem('city'));
+
+  // for (i=0; i <= ".button".length; i++) {
+
+  // }
 
 }
 
