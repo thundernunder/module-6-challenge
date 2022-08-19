b410e3c6f6409d1bef7db62f381401e516595db7
Nunder <nathannunderwood@gmail.com>
2022-08-18 22:29:35 -0700

    Add script-2

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,17 +1,18 @@
-var userInput = $("#input-text");
-var key = api.key;
-var cities = [];
-var buttonEl = $('button');
+var searchHistory = [];
+var apiKey = api.key;
+var rootWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=limit=5&units=imperial&appid=${apiKey}`;
+var userInput = document.getElementById('search-bar');
+var submitBtn = document.getElementById('submitButton');
 
 
-function getWeather() {
-  var city = userInput.val();
 
+function getWeatherToday() {
+  var city = userInput.value;
   fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + api.key)
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     console.log(data);
     var location = $('#city-name');
     location.text(data.city.name);
@@ -22,68 +23,11 @@ function getWeather() {
     var humidity = $('#humidity');
     humidity.text(data.list[0].main.humidity);
 
-    cities.push(userInput.val());
-    localStorage.setItem('cities', cities);
-    console.log(cities);
-    makeHistoryButton();
-    
-     
-  }); 
-  
-  
-}
-
-function makeHistoryButton() {
-  var rootEl = $('#search-buttons');
-  var historyButton = $('<button>');
-  historyButton.attr('class', 'dynamicButton');
-  historyButton.attr('id', userInput.val());
-  historyButton.attr('value', userInput.val());
-  rootEl.append(historyButton);
-  historyButton.text(userInput.val());
-  console.log(userInput.val());
+    searchHistory.push(userInput.val());
+    localStorage.setItem('Search History', searchHistory);
+    console.log(searchHistory);
+  });
 };
 
-var historyButton = $('<button>');
-historyButton.on('click', function() {
-  var singleButton = historyButton[i];
-  for (i=0; i <= historyButton.length; i++) {
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + historyButton[i].val() + "&units=imperial&appid=" + api.key)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-    var location = $('#city-name');
-    location.text(data.city.name);
-    var temp = $('#temperature');
-    temp.text(data.list[0].main.temp);
-    var wind = $('#wind');
-    wind.text(data.list[0].wind.speed);
-    var humidity = $('#humidity');
-    humidity.text(data.list[0].main.humidity);
-  })
-  }
-  
-})
-
-
-
-
-var cityButton = $("#submitButton");
-cityButton.on('click', getWeather);
-
-
-
-  
-   
-
-
-
-  
-
-  
-  
-
-
+submitBtn.addEventListener('click', getWeatherToday);
 
---
+++ Assets/js/script-two.js
@@ -0,0 +1,66 @@
+var userInput = $("#input-text");
+var key = api.key;
+var searchHistory = [];
+var buttonEl = $('button');
+
+
+function getWeather() {
+  var city = userInput.val();
+
+  fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + api.key)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    var location = $('#city-name');
+    location.text(data.city.name);
+    var temp = $('#temperature');
+    temp.text(data.list[0].main.temp);
+    var wind = $('#wind');
+    wind.text(data.list[0].wind.speed);
+    var humidity = $('#humidity');
+    humidity.text(data.list[0].main.humidity);
+
+    searchHistory.push(userInput.val());
+    localStorage.setItem('searchHistory', searchHistory);
+    console.log(searchHistory);
+    makeHistoryButton();
+  }); 
+}
+
+function makeHistoryButton() {
+  var rootEl = $('#search-buttons');
+  var historyButton = $('<button>');
+  historyButton.attr('class', 'dynamicButton');
+  historyButton.attr('id', userInput.val());
+  historyButton.attr('value', userInput.val());
+  rootEl.append(historyButton);
+  historyButton.text(userInput.val());
+  console.log(userInput.val());
+};
+
+var historyButton = $('<button>');
+historyButton.on('click', function() {
+  var singleButton = historyButton[i];
+  for (i=0; i <= historyButton.length; i++) {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + historyButton[i].val() + "&units=imperial&appid=" + api.key)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    var location = $('#city-name');
+    location.text(data.city.name);
+    var temp = $('#temperature');
+    temp.text(data.list[0].main.temp);
+    var wind = $('#wind');
+    wind.text(data.list[0].wind.speed);
+    var humidity = $('#humidity');
+    humidity.text(data.list[0].main.humidity);
+  })
+  }  
+})
+
+var cityButton = $("#submitButton");
+cityButton.on('click', getWeather);
