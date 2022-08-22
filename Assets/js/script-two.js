63146ec3813319739c532302764d7e877a3f13b3
Nunder <nathannunderwood@gmail.com>
2022-08-22 13:28:34 -0700

    Five day and button done. Need icons.

--- Assets/js/script-two.js
+++ Assets/js/script-two.js
@@ -17,11 +17,11 @@ function getWeather() {
     var location = $('#city-name');
     location.text(data.city.name);
     var temp = $('#temperature');
-    temp.text(data.list[0].main.temp);
+    temp.text("Temp: " + data.list[0].main.temp);
     var wind = $('#wind');
-    wind.text(data.list[0].wind.speed);
+    wind.text("Wind: " + data.list[0].wind.speed + "MPH");
     var humidity = $('#humidity');
-    humidity.text(data.list[0].main.humidity);
+    humidity.text("Humidity: " + data.list[0].main.humidity + "%");
 
     var cityName = data.city.name;
     console.log(cityName);
@@ -38,16 +38,38 @@ function getWeather() {
     for (i=0; i < 39; i += 8) {
       var weatherSection = $('#forecast');
       var weatherCard = document.createElement('div');
-      var cardHeading = document.createElement('h3');
+      var cardDate = document.createElement('h3');
       var cardTemp = document.createElement('p');
       var cardWind = document.createElement('p');
       var cardHumidity = document.createElement('p');
-
-      cardHeading.textContent = data.list[i].main.temp;
+      
+      cardDate.textContent = data.list[i].dt_txt;
+      cardTemp.textContent = "Temp: " + data.list[i].main.temp;
+      cardWind.textContent = "Wind: " + data.list[i].wind.speed + "MPH";
+      cardHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
 
       weatherSection.append(weatherCard);
-      weatherCard.append(cardHeading);
-    }
+      weatherCard.append(cardDate);
+      weatherCard.append(cardTemp);
+      weatherCard.append(cardWind);
+      weatherCard.append(cardHumidity);
+
+  };
+  var cityLat = data.city.coord.lat;
+      var cityLon = data.city.coord.lon;
+      var uvURl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + api.key;
+
+    fetch(uvURl, {})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+    var cardUVi = document.getElementById('uvi');
+    cardUVi.textContent = "UVI Index: " + data.value;
+    
+  
   });
   });
 
@@ -56,7 +78,8 @@ function getWeather() {
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
     console.log(searchHistory);
     makeHistoryButton();
-}; 
+})}; 
+
 
 function makeHistoryButton() {
   var rootEl = $('#search-buttons');
@@ -85,13 +108,60 @@ $('#search-buttons').on('click', '.dynamicButton',function(){
     var location = $('#city-name');
     location.text(data.city.name);
     var temp = $('#temperature');
-    temp.text("Temp"+ data.list[0].main.temp);
+    temp.text("Temp: "+ data.list[0].main.temp);
     var wind = $('#wind');
-    wind.text(data.list[0].wind.speed);
+    wind.text("Wind " + data.list[0].wind.speed + "MPH");
     var humidity = $('#humidity');
-    humidity.text(data.list[0].main.humidity);
-  })
-});
+    humidity.text(data.list[0].main.humidity + "%");
+
+    var cityName = data.city.name;
+    console.log(cityName);
+
+    var fiveDayQuery = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + api.key;
+
+    fetch(fiveDayQuery, {})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+    for (i=0; i < 39; i += 8) {
+      var weatherSection = $('#forecast');
+      var weatherCard = document.createElement('div');
+      var cardDate = document.createElement('h3');
+      var cardTemp = document.createElement('p');
+      var cardWind = document.createElement('p');
+      var cardHumidity = document.createElement('p');
+      
+      cardDate.textContent = data.list[i].dt_txt;
+      cardTemp.textContent = "Temp: " + data.list[i].main.temp;
+      cardWind.textContent = "Wind: " + data.list[i].wind.speed + "MPH";
+      cardHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+
+      weatherSection.append(weatherCard);
+      weatherCard.append(cardDate);
+      weatherCard.append(cardTemp);
+      weatherCard.append(cardWind);
+      weatherCard.append(cardHumidity);
+    }});
+    var cityLat = data.city.coord.lat;
+      var cityLon = data.city.coord.lon;
+      var uvURl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + api.key;
+
+    fetch(uvURl, {})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+    var cardUVi = document.getElementById('uvi');
+    cardUVi.textContent = "UVI Index: " + data.value;
+    }
+)})}
+);
+
 
 
 
